41550941c04ee89592c18a4f6e560e67ca003e21
emrahgingir <emrahgingir@cloud.upwork.com>
2025-03-17 04:43:46 +0300

    ad

--- src/time.js
+++ src/time.js
@@ -1,5 +1,12 @@
+/**
+ * Pads a number to 2 digits.
+ * @param {string} date - The input string.
+ * @returns {string} The padded string.
+ */
 export function getHoursMinutesFromDate(date) {
   if (typeof date === "string") date = new Date(date);
 
   return ("0" + date.getHours()).slice(-2) + ":" + ("0" + date.getMinutes()).slice(-2);
 }
+
+export const timeUtils = { getHoursMinutesFromDate };
